7486527704e2e09cc71a2ae02e07ad33b33d84eb
mm438926 <m.nagase1031@gmail.com>
2020-12-15 15:21:25 +0900

    second

--- calculate.js
+++ calculate.js
@@ -1,16 +1,27 @@
 'use strict';
 
-let total = 0;
-let input = "";
-let calc = "+";
-let flag = 1;
+let total = 0;      // 現在の合計値
+let input = "";     // 現在入力している値
+let calc = "+";     // 合計と入力値の演算
+let flag = 1;       // １回前に入力したもの 0:数字 1:演算子
 
+//数字の入力
 function clic(data){
   flag = 0;
   input += data;
   document.form.text.value = input;
 }
 
+//小数点を１回以上押せない
+function period(data){
+  if (input.slice(-1) == data) {
+    return;
+  }  //変数inputの一つ前が'.' slice＝文字列切抜
+  input += data;
+  document.form.text.value = input;
+}
+
+//計算
 function enzan(data){
   if (flag==0){
     flag = 1;
@@ -27,9 +38,15 @@ function enzan(data){
   }
 }
 
-function clear(){
-  total = 0;
-  input = "";
-  calc = "+"
-  document.form.text.value = total;
+//クリア
+function kesu(data){
+  if (data == 'AC'){
+    total = 0;
+    input = "";
+    calc = "+"
+    document.form.text.value = total;
+  } else if (data == 'C') {
+    input = "";
+    document.form.text.value = document.form.text.value.slice(0, -1);
+  } //１つ前の動作を表示しない？動作自体なかったことにする？
 }
